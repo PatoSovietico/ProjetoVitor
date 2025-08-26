0eb32a6cdf6277ff735c451dbec9eea7f89132d7
PatoSovietico <arthurbat120907@gmail.com>
2025-08-26 12:35:34 -0400

    cu

---
+++ src/modules/scenario/use-cases/create-scenario.use-case.ts
@@ -0,0 +1,26 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { CreateScenarioRepository } from "../repository/create-scenario.repository";
+import { CreateScenarioDto } from "../dto/create-scenario.dto";
+
+ 
+
+ @Injectable()
+ export class CreateScenarioUseCase {
+    constructor(
+        private readonly createScenarioRepository: CreateScenarioRepository,
+        private readonly logger: Logger,
+    ) {}
+
+    async execute(data: CreateScenarioDto){
+        try{
+            const scenario = this. createScenarioRepository.create(data);
+        } catch (error){
+            this.logger.error(error);
+            throw error;
+        }
+
+
+
+
+    }
+ }
